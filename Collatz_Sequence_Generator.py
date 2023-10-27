e9c446c60fd2ce127c38eb18d20e2d66ddd45dc1
Kooreis <kooreis.official@gmail.com>
2023-10-26 19:36:09 -0500

    Defined the function collatz_sequence that takes an integer n as input and starts a while loop that continues until n is not equal to 1. The current value of n is printed in each iteration.

--- Collatz_Sequence_Generator.py
+++ Collatz_Sequence_Generator.py
@@ -1,11 +1,3 @@
 def collatz_sequence(n):
     while n != 1:
         print(n)
-        if n % 2 == 0:
-            n = n // 2
-        else:
-            n = 3*n + 1
-    print(n)
-
-n = int(input("Enter a number: "))
-collatz_sequence(n)
